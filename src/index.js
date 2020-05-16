8ea82564e008b11eded203d2acbc0984bfc48e9a
KirillZhdanov <kirill.zhdanov97@gmail.com>
2020-05-16 15:09:54 +0300

    feat: refactoring

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import cards from './cards.js';
 
+const CARD_NUMBER = 8;
+const startGame = 'Start game';
+const REPEAT = 'repeat';
+const MENU_ITEM = 'menu-item';
+const soundSuccess = './sounds/success.mp3';
+const soundError = './sounds/error.mp3';
+const soundLose = './sounds/lose.mp3';
+const soundCorrect = './sounds/correct.mp3';
+const SUCCESSPIC = './img/success.jpg';
+const FAILUREPIC = './img/failure.jpg';
+
 function gameText() {
-  const test = document.querySelectorAll('div.card-action');
+  const cardActions = document.querySelectorAll('div.card-action');
   const startBtn = document.querySelector('.start');
   if (isGamemode && index > 0) {
-    startBtn.removeAttribute('style');
-    for (const el of test) {
-      el.style.display = 'none';
+    startBtn.classList.remove('hide');
+    for (const el of cardActions) {
+      el.classList.add("hide");
     }
   }
 }
 
-const elems = document.querySelectorAll('.sidenav');
-M.Sidenav.init(elems, {});
-function soundClick(filename = './sounds/test.mp3') {
+document.addEventListener("DOMContentLoaded", function(){
+  const elems = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elems, {});
+});
+
+function soundClick(filename = './sounds/fly.mp3') {
   const audio = new Audio();
   audio.src = filename;
   audio.autoplay = true;
@@ -22,84 +36,65 @@ function soundClick(filename = './sounds/test.mp3') {
 let index; let isGamemode = false;
 localStorage.setItem('state', 'false');
 class Page {
-  create(id, pics, translation) {
+  createCard(id, pics, translation) {
     const container = document.createElement('div');
     container.className = 'col s12 m6 l3 card-container';
     container.id = id;
     const row = document.querySelector('.row');
-    const imageFront = document.createElement('img');
-    const imageBack = document.createElement('img');
     const card = document.createElement('div');
     card.id = `card_${id}`;
     card.className = 'card';
-    // front side of card
-    const cardFront = document.createElement('div');
-    cardFront.id = id;
-    cardFront.className = 'card-front';
-    const cardimageFront = document.createElement('div');
-    cardimageFront.id = id;
-    cardimageFront.className = 'card-image';
-    const cardActionFront = document.createElement('div');
-    cardActionFront.className = 'card-action';
-    cardActionFront.id = id;
-    const cardActionFrontText = document.createElement('span');
-    cardActionFrontText.innerHTML = id.toUpperCase();
-    cardActionFrontText.id = id;
-    cardActionFrontText.className = 'word';
-    const repeatF = document.createElement('img');
-    repeatF.src = './img/repeat.png';
-    repeatF.id = id;
-    repeatF.className = 'repeat';
-    // back side of card
-    const cardBack = document.createElement('div');
-    cardBack.id = id;
-    cardBack.className = 'card-back';
-    const cardimageBack = document.createElement('div');
-    cardimageBack.id = id;
-    cardimageBack.className = 'card-image';
-    const cardActionBack = document.createElement('div');
-    cardActionBack.className = 'card-action';
-    const cardActionBackText = document.createElement('span');
-    cardActionBackText.innerHTML = translation.toUpperCase();
-    cardActionBackText.id = id;
-    cardActionBackText.className = 'word';
-
-
-    imageFront.id = id;
-    imageFront.src = pics;
-
-    imageBack.id = id;
-    imageBack.src = pics;
-
-    cardimageFront.appendChild(imageFront);
-    cardFront.appendChild(cardimageFront);
-    cardActionFront.appendChild(cardActionFrontText);
-    cardActionFront.appendChild(repeatF);
-    cardFront.appendChild(cardActionFront);
-    card.appendChild(cardFront);
+    card.appendChild(this.createCardSide(id, this.createImage(id, pics), id, 'front'));
     container.appendChild(card);
-
-    cardimageBack.appendChild(imageBack);
-    cardBack.appendChild(cardimageBack);
-    cardActionBack.appendChild(cardActionBackText);
-    cardBack.appendChild(cardActionBack);
-    card.appendChild(cardBack);
+    card.appendChild(this.createCardSide(id, this.createImage(id, pics), translation, 'back'));
     container.appendChild(card);
-
     row.appendChild(container);
   }
-
+  createCardSide(id, image, word ,side){
+    const card = document.createElement('div');
+    card.id = id;
+    card.className = `card-${side}`;
+    const cardimage = document.createElement('div');
+    cardimage.id = id;
+    cardimage.className = 'card-image';
+    const cardAction = document.createElement('div');
+    cardAction.className = 'card-action';
+    cardAction.id = id;
+    const cardActionText = document.createElement('span');
+    cardActionText.innerHTML = word.toUpperCase();
+    cardActionText.id = id;
+    cardActionText.className = 'word';
+    cardimage.appendChild(image);
+    card.appendChild(cardimage);
+    cardAction.appendChild(cardActionText);
+    if(side === 'front'){
+      const repeatF = document.createElement('img');
+      repeatF.src = `./img/${REPEAT}.png`;
+      repeatF.id = id;
+      repeatF.className = REPEAT;
+      cardAction.appendChild(repeatF);
+    }
+    card.appendChild(cardAction);
+    return card;
+  }
+  createImage(id, pics){
+    const image = document.createElement('img');
+    image.id = id;
+    image.src = pics;
+    return image;
+  }
   addStart() {
     const body = document.querySelector('body');
     const startButton = document.createElement('button');
-    startButton.textContent = 'Start game';
+    startButton.textContent = startGame;
     startButton.className = 'waves-effect waves-light btn start';
     startButton.type = 'button';
-    startButton.style.display = 'none';
+    startButton.classList.add('hide');
     body.appendChild(startButton);
   }
 }
 class MainPage {
+  
   create(id, pics) {
     const container = document.createElement('div');
     container.className = 'col s12 m6 l3 card-container';
@@ -108,18 +103,18 @@ class MainPage {
     const image = document.createElement('img');
     const card = document.createElement('div');
     card.id = id;
-    card.className = 'card menu-item';
+    card.className = `card ${MENU_ITEM}`;
     const cardimage = document.createElement('div');
     cardimage.id = id;
-    cardimage.className = 'card-image menu-item teal';
+    cardimage.className = `card-image ${MENU_ITEM} teal`;
     const cardAction = document.createElement('div');
-    cardAction.className = 'card-action menu-item';
+    cardAction.className = `card-action ${MENU_ITEM}`;
     cardAction.id = id;
     const cardActionText = document.createElement('p');
-    cardAction.className = 'menu-item';
+    cardAction.className = MENU_ITEM;
     cardActionText.innerHTML = id.toUpperCase();
     image.id = id;
-    image.className = 'menu-item';
+    image.className = MENU_ITEM;
     image.src = pics;
     cardimage.appendChild(image);
     cardAction.appendChild(cardActionText);
@@ -128,11 +123,12 @@ class MainPage {
     container.appendChild(card);
     row.appendChild(container);
   }
+ 
 }
 const page = new Page();
 const mainPage = new MainPage();
 index = 0;
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < CARD_NUMBER; i++) {
   mainPage.create(cards[0][i], cards[i + 1][index + 1].image);
 }
 
@@ -157,26 +153,21 @@ function gameSound() {
   document.querySelector('.start').addEventListener('click', () => {
     soundClick(soundGame);
   });
-  const soundSuccess = './sounds/success.mp3';
-  const soundError = './sounds/error.mp3';
-  const soundLose = './sounds/lose.mp3';
-  const soundCorrect = './sounds/correct.mp3';
+  
   const row = document.querySelector('.row');
   const stars = document.querySelector('.stars');
   document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('menu-item')) {
-      while (stars.firstChild) {
-        stars.removeChild(stars.firstChild);
-      }
+    if (event.target.classList.contains(MENU_ITEM)) {
+      stars.innerHTML = '';
       errors = 0;
       soundGame = '';
-      document.querySelector('.start').textContent = 'Start game';
-      document.querySelector('.start').style.display = 'none';
+      document.querySelector('.start').textContent = startGame;
+      document.querySelector('.start').classList.add('hide');
     }
     if (event.target.id === a && playlist.length > 0) {
       soundClick(soundCorrect);
       a = playlist.pop();
-      event.target.closest('.card-container').classList.toggle('grayFilt');
+      event.target.closest('.card-container').classList.add('grayFilt');
       const starwin = document.createElement('img');
       starwin.src = './img/star-win.png';
       stars.prepend(starwin);
@@ -191,30 +182,26 @@ function gameSound() {
       stars.prepend(starlose);
     }
     if (event.target.id === a && playlist.length === 0) {
-      while (row.firstChild) {
-        row.removeChild(row.firstChild);
-      }
+      row.innerHTML = '';
       const resultpic = document.createElement('img');
 
       if (errors === 0) {
         soundClick(soundSuccess);
-        resultpic.src = './img/success.jpg';
+        resultpic.src = SUCCESSPIC;
       } else {
         soundClick(soundLose);
-        resultpic.src = './img/failure.jpg';
+        resultpic.src = FAILUREPIC;
         const resultErrors = document.createElement('p');
         resultErrors.innerHTML = `Errors: ${errors}`;
         row.appendChild(resultErrors);
       }
-      while (stars.firstChild) {
-        stars.removeChild(stars.firstChild);
-      }
+      stars.innerHTML = '';
       resultpic.className = 'center';
       row.appendChild(resultpic);
       errors = 0;
       soundGame = '';
-      document.querySelector('.start').textContent = 'Start game';
-      document.querySelector('.start').style.display = 'none';
+      document.querySelector('.start').textContent = startGame;
+      document.querySelector('.start').classList.add('hide');
       setTimeout(() => {
         location.reload();
       }, 3000);
@@ -223,14 +210,14 @@ function gameSound() {
 }
 
 document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('start') && event.target.textContent !== 'repeat') {
-    event.target.textContent = 'repeat';
+  if (event.target.classList.contains('start') && event.target.textContent !== REPEAT) {
+    event.target.textContent = REPEAT;
     if (isGamemode && index > 0) {
       gameSound();
     }
   }
 
-  if (event.target.classList.contains('menu-item')) {
+  if (event.target.classList.contains(MENU_ITEM)) {
     const { target } = event;
     index = cards[0].indexOf(target.id) + 1;
     document.querySelectorAll('a').forEach((element) => element.classList.remove('active'));
@@ -241,11 +228,9 @@ document.addEventListener('click', (event) => {
 
     if (index > 0 && index !== 'undefined') {
       const row = document.querySelector('.row');
-      while (row.firstChild) {
-        row.removeChild(row.firstChild);
-      }
-      for (let i = 0; i < 8; i++) {
-        page.create(cards[index][i].word, cards[index][i].image, cards[index][i].translation);
+      row.innerHTML = '';
+      for (let i = 0; i < CARD_NUMBER; i++) {
+        page.createCard(cards[index][i].word, cards[index][i].image, cards[index][i].translation);
       }
       const btns = document.querySelectorAll('.start');
       if (btns.length < 1) { page.addStart(); }
@@ -268,7 +253,7 @@ document.addEventListener('click', (event) => {
     }
     return;
   }
-  if (event.target.id !== '' && !event.target.classList.contains('menu-item') && !isGamemode) {
+  if (event.target.id !== '' && !event.target.classList.contains(MENU_ITEM) && !isGamemode) {
     const sound = `./sounds/${event.target.id}.mp3`;
     soundClick(sound);
   }
@@ -283,15 +268,15 @@ switcher.addEventListener('click', () => {
     bodyColor.classList.toggle('gameBackground');
     isGamemode = true;
     for (const el of cardAction) {
-      el.style.display = 'none';
+      el.classList.add('hide');
     }
-    if (index > 0) { startBtn.removeAttribute('style'); }
+    if (index > 0) { startBtn.classList.remove('hide'); }
   } else {
     bodyColor.classList.toggle('gameBackground');
     isGamemode = false;
     for (const el of cardAction) {
-      el.removeAttribute('style');
+      el.classList.remove('hide');
     }
-    if (index > 0) { startBtn.style.display = 'none'; }
+    if (index > 0) { startBtn.classList.add('hide'); }
   }
 });
